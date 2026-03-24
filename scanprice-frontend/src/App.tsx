4ce7e9e06038d76430833a8dd1b4a1b3993f0247
ClaudioDevv <claudiorivasns@gmail.com>
2026-03-24 19:27:13 +0100

    feat: Inicializar frontend

---
+++ scanprice-frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <section className='main'>
+        <h1>App de prácticas de Ingeniería de Sistemas de Información</h1>
+        <ul>
+          <li className='student'>Claudio Rivas</li>
+          <li className='student'>Yeray Ortega</li>
+        </ul>
+    </section>
+
+  )
+}
+
+export default App
