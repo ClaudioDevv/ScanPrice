4a7c4119241eb7f6cac6aacc1a94bf771c9ee9ba
ClaudioDevv <claudiorivasns@gmail.com>
2026-03-27 16:08:26 +0100

    feat: Automatizacion CD GitHub Actions

--- scanprice-frontend/src/App.tsx
+++ scanprice-frontend/src/App.tsx
@@ -6,8 +6,8 @@ function App() {
     <section className='main'>
         <h1>App de prácticas de Ingeniería de Sistemas de Información</h1>
         <ul>
-          <li className='student'>Claudio Rivas</li>
-          <li className='student'>Yeray Ortega</li>
+          <li className='student'>Claudio Rivas Boza</li>
+          <li className='student'>Yeray Ortega Fernández</li>
         </ul>
     </section>
 
